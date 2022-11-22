e2eea06062de449b494f15155047164dc1786906
lx64 <artxak8980@gmail.com>
2022-11-22 16:27:36 +0300

    подписал начало и конец работы в файле main

--- func_copy_and_filter/main.py
+++ func_copy_and_filter/main.py
@@ -4,7 +4,9 @@ from func_copy_and_filter.filter_values import *
 
 if __name__ == '__main__':
     """=====Запуск всех методов!===="""
+    print("==========================start to working==========================")
     print(copy_passwd())
     print(copy_group())
     print(filter_users())
     print(filter_group())
+    print("==========================end to work==========================")
